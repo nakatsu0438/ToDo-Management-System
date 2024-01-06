fd0887e6c4bc672c25458dc29418e2344b2a92d5
YutaNakatsu <https://github.com/nakatsu0438/eclipse-test.git>
2024-01-07 03:27:06 +0900

    質問に付きプッシュ

--- src/main/java/com/dmm/task/entity/Tasks.java
+++ src/main/java/com/dmm/task/entity/Tasks.java
@@ -1,32 +1,30 @@
 package com.dmm.task.entity;
 
+import java.time.LocalDate;
 
-
-//import javax.persistence.Column;
-
-
-
-import javax.persistence.Entity;
-
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-import javax.persistence.Id;
-//import javax.persistence.ManyToOne;
-//import javax.persistence.Table;
-//import javax.persistence.JoinColumn;
+import javax.persistence.*;
 
 import lombok.Data;
 import lombok.ToString;
-import java.time.LocalDate;
 
 @Data
 @Entity
 @ToString
 public class Tasks {
-	@Id
-	 private Long id;
-	 private String user;
-	 private String name;
-	 private boolean done;
-	 private LocalDate date;
+    @Id
+    @GeneratedValue
+    private Long id;
+    
+    @ManyToOne
+    @JoinColumn(name = "user_name")
+    private Users user;
+
+    @Column
+    private String name;
+    
+    @Column
+    private boolean done;
+    
+    @Column
+    private LocalDate date;
 }
